912ae452ce4610fb5209927863a405af671d9042
Mário Gil <mg_mesquita@hotmail.com>
2018-07-20 16:26:48 +0100

    Scrolling animations

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,3 +1,10 @@
 $('.carousel').carousel({
   interval: 4000
 })
+
+$("a").on("click", function() {
+	let ref = $(this).attr("href");
+	$('html, body').animate({
+	    scrollTop: $(ref).offset().top
+	}, 1000);
+});
