aedff67db7db755d5b7ebbf0134d1e6525d90deb
andrefmrocha <andrefmrocha@live.com.pt>
2018-07-20 17:58:44 +0100

    Fixed services

--- Assets/js/services.js
+++ Assets/js/services.js
@@ -1,19 +1,34 @@
 $(function () {
     let hash = $(location).attr('hash');
+
     let offset = $($(location).attr('hash')).offset();
-    $('html, body').animate({
-        scrollTop: offset.top
-    }, 1000);
-    $(hash).addClass("slideAppear");
+    if (offset){
+        $('html, body').animate({
+            scrollTop: offset.top
+        }, 1000);
+        $(hash).addClass("slideAppear");
+    }
+    else
+    {
+        $("#accounting").addClass("slideAppear");
+    }
 
-        $(window).scroll(function() {
-        $(".slideanim").each(function(){
-            var pos = $(this).offset().top;
+    $("a").on("click", function() {
+        let ref = $(this).attr("href");
+        console.log($(ref).offset());
+        $('html, body').animate({
+            scrollTop: $(ref).offset().top
+        }, 1000);
+    });
 
-            var winTop = $(window).scrollTop();
-            if (pos < winTop + 600) {
-                $(this).addClass("slideAppear");
-            }
+    $(window).scroll(function() {
+    $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+            $(this).addClass("slideAppear");
+        }
         });
     });
 
